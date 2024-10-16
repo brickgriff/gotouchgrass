ab026498d9b77603ba0f7f72a9243a8cd2852cc1
bgriffiniv <bgriffiniv@gmail.com>
2024-10-16 10:26:20 -0400

    major update v009
    touch support

--- events.js
+++ events.js
@@ -49,6 +49,77 @@ window.addEventListener("mousemove", (e) =>{
   }
 });
 
+window.addEventListener("touchstart",(e)=>{
+  for(let i=0; i<e.changedTouches.length; i++) {
+    pushKey({keys:state.keys, key:ongoingTouches.length});
+    ongoingTouches.push(copyTouch(e.touches[i]));
+  }
+
+  if (ongoingTouches.length > keybinds.mouseL) return;
+
+  state.mouse.x=e.screenX;
+  state.mouse.y=e.screenY;
+
+  state.mouse._x=state.mouse.x;
+  state.mouse._y=state.mouse.y;
+});
+
+var handleTouchFinish = (e)=>{
+  for(let i=0; i<e.changedTouches.length; i++) {
+    let index = ongoingTouchIndexById(e.changedTouches.identifier);
+    //let touch=ongoingTouches[index];
+    if (index===keybinds.mouseL) {
+      state.mouse.x=state.player.x;
+      state.mouse.y=state.player.y;
+      state.mouse._x=state.mouse.x; 
+      state.mouse._y=state.mouse.y;
+      state.mouse.isDragged = false;
+    }
+
+    dropKey({key:index,keys:state.keys});
+    ongoingTouches.splice(index,1);
+  }
+};
+
+window.addEventListener("touchend",handleTouchFinish);
+window.addEventListener("touchcancel",handleTouchFinish);
+
+window.addEventListener("touchmove",(e)=>{
+  for(let i=0; i<e.changedTouches.length; i++) {
+    let index = ongoingTouchIndexById(e.changedTouches.identifier);
+    //let touch=ongoingTouches[index];
+    if (index===keybinds.mouseL) {
+      state.mouse._x=e.offsetX;
+      state.mouse._y=e.offsetY;
+
+      let dist = Math.hypot(state.mouse._x-state.mouse.x,state.mouse._y-state.mouse.y);
+      state.mouse.isDragged = (dist>=state.player.dg);
+      //console.log(dist,state.mouse.isDragged);
+    }
+
+    dropKey({key:index,keys:state.keys});
+    ongoingTouches.splice(index,1);
+  }
+
+});
+
+const ongoingTouches = [];
+
+function copyTouch({ identifier, pageX, pageY }) {
+  return { identifier, pageX, pageY };
+}
+
+function ongoingTouchIndexById(idToFind) {
+  for (let i = 0; i < ongoingTouches.length; i++) {
+    const id = ongoingTouches[i].identifier;
+
+    if (id === idToFind) {
+      return i;
+    }
+  }
+  return -1; // not found
+}
+
 /* --- */
 
 /* PLAYER */
