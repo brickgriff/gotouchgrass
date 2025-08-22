5a3ce3bdf1f4ed6097899fa947889d57f83b97e6
brickgriff <bgriffiniv@gmail.com>
2025-08-22 18:34:35 -0400

    the beginning of the start

--- main.js
+++ main.js
@@ -6,38 +6,38 @@ function mainLoop(now) {
   const state = document.state;
 
   // time tracking
-  if (!state.frame) state.frame = 0; // init frame
+  if (state.frame == undefined) state.frame = 0; // init frame
   const elapsed = (now - (state.time || now)); // deltaTime in millis
   const dt = elapsed > 1000 ? 1000 : elapsed; // cap deltaTime @ 1000ms
 
-  //console.log(`gameLoop(now=${now}, frame=${state.frame++}, deltaTime=${dt}, framesPerSecond=${dt==0?"START":Math.floor(1000/dt)})`);
+  // console.log(`gameLoop(now=${now}, frame=${state.frame++}, deltaTime=${dt}, framesPerSecond=${dt==0?"START":Math.floor(1000/dt)})`);
 
+
+  /* most of this code should go in world.js and should only run on a resize event */
   // minimum-dimension
   const mindim = Math.min(self.innerWidth, self.innerHeight);
   state.canvas.width = self.innerWidth;
   state.canvas.height = self.innerHeight;
-
   const ctx = state.ctx;
   const cx = state.canvas.width / 2;
   const cy = state.canvas.height / 2;
-
-  ctx.translate(cx, cy);
+  ctx.translate(cx, cy); // minimize translate calls 
 
   // init displacement vector
   if (state.dx == undefined || state.dy == undefined) { state.dx = 0; state.dy = 0; }
-  const speed = 0.005; // some percent of mindim
+  const speed = 0.005; // some percent of mindim (0.5%)
 
-  const vector = {};
+  const vx = (state.inputs.buttons.includes(keybinds.right) - state.inputs.buttons.includes(keybinds.left));
+  const vy = (state.inputs.buttons.includes(keybinds.down) - state.inputs.buttons.includes(keybinds.up));
 
-  vector.x = (state.inputs.buttons.includes(keybinds.right) - state.inputs.buttons.includes(keybinds.left));
-  vector.y = (state.inputs.buttons.includes(keybinds.down) - state.inputs.buttons.includes(keybinds.up));
-
-  const displacement = Math.hypot(vector.x, vector.y);
-  const angle = Math.atan2(vector.y, vector.x);
+  const displacement = Math.hypot(vx, vy);
+  const angle = Math.atan2(vy, vx);
 
   state.dx -= speed * (displacement == 0 ? 0 : 1) * Math.cos(angle);
   state.dy -= speed * (displacement == 0 ? 0 : 1) * Math.sin(angle);
 
+
+  /* this code should go in display.js */
   // draw background
   ctx.fillStyle = "dimgray";
   ctx.fillRect(-cx, -cy, state.canvas.width, state.canvas.height);
@@ -71,8 +71,8 @@ function mainLoop(now) {
 
   ctx.beginPath();
   ctx.strokeStyle = "black";
-  ctx.moveTo(0 + 0.5*mindim, 0);
-  ctx.arc(0, 0, 0.5*mindim, 0, Math.PI * 2);
+  ctx.moveTo(0 + 0.5 * mindim, 0);
+  ctx.arc(0, 0, 0.5 * mindim, 0, Math.PI * 2);
   ctx.stroke();
 
   //World.update(state, dt); // update entities
