1ca97c0ffccfaa9a2eaba8e2f473baa138333eb5
brickgriff <bgriffiniv@gmail.com>
2025-08-23 17:14:51 -0400

    sprng

---
+++ sprng.js
@@ -0,0 +1,11 @@
+const Random = (function (/*api*/) {
+    var api = {};
+
+    // take in a string
+    // translate that as a number
+    // use that as an angle in a sin call
+    // take the decimal part
+    // use that as your next number
+
+    return api;
+}());
