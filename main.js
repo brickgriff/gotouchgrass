cb435b08870e9b613d32d154d08a6c917a888880
brickgriff <bgriffiniv@gmail.com>
2025-08-22 21:55:25 -0400

    player walks on nearby grass --- up next: - feedback: walking and touching

--- main.js
+++ main.js
@@ -12,46 +12,22 @@ function mainLoop(now) {
 
   // console.log(`gameLoop(now=${now}, frame=${state.frame++}, deltaTime=${dt}, framesPerSecond=${dt==0?"START":Math.floor(1000/dt)})`);
 
-  /* most of this code should go in world.js and should only run on a resize event */
-  // minimum-dimension
-  const mindim = Math.min(self.innerWidth, self.innerHeight);
-  state.canvas.width = self.innerWidth;
-  state.canvas.height = self.innerHeight;
+  // TODO: switch back
+  // world can create entities
+  // display can draw them
+  // they both share player position and velocity
+  World.update(dt); // update entities
+  //Display.draw(state, state.ctx); // draw entities
+
+  /* this code should go in display.js */
   const ctx = state.ctx;
   const cx = state.canvas.width / 2;
   const cy = state.canvas.height / 2;
-  ctx.translate(cx, cy); // minimize translate calls 
+  const mindim = Math.min(self.innerWidth, self.innerHeight);
 
-  // init displacement vector
-  if (state.dx == undefined || state.dy == undefined) { state.dx = 0; state.dy = 0; }
-  const speed = 0.005; // some percent of mindim (0.5%)
-
-  const vx = (state.inputs.buttons.includes(keybinds.right) - state.inputs.buttons.includes(keybinds.left));
-  const vy = (state.inputs.buttons.includes(keybinds.down) - state.inputs.buttons.includes(keybinds.up));
-
-  const displacement = Math.hypot(vx, vy);
-  const angle = Math.atan2(vy, vx);
-
-  state.dx -= speed * (displacement == 0 ? 0 : 1) * Math.cos(angle);
-  state.dy -= speed * (displacement == 0 ? 0 : 1) * Math.sin(angle);
-
-  if (state.plants == undefined) {
-    const plants = [];
-    var num = 10000;
-    while (num--) {
-      let x1 = (Math.random()*3-1.5);
-      let y1 = (Math.random()*3-1.5);
-      let r1 = (Math.random()*.6+.4)*0.025;
-      plants.push({x:x1,y:y1,r:r1});  
-    }
-    state.plants = plants;
-  }
-
-  /* this code should go in display.js */
   // draw background
   ctx.fillStyle = "dimgray";
   ctx.fillRect(-cx, -cy, state.canvas.width, state.canvas.height);
-
 
   // draw plant circles
   ctx.beginPath();
@@ -64,24 +40,25 @@ function mainLoop(now) {
 
   for (plant of state.plants) {
 
-    x=(plant.x + state.dx) * mindim;
-    y=(plant.y + state.dy) * mindim;
-    r=plant.r * mindim;
-  
-    if (Math.hypot(x, y) > .1*mindim) {
+    x = (plant.x + state.dx) * mindim;
+    y = (plant.y + state.dy) * mindim;
+    r = plant.r * mindim;
+
+    if (Math.hypot(x, y) > .1 * mindim) {
       continue;
     }
 
-    ctx.moveTo(x+r,y);
-    ctx.arc(x,y,r,0,Math.PI*2);
+    ctx.moveTo(x + r, y);
+    ctx.arc(x, y, r, 0, Math.PI * 2);
   }
+
   x = (.1 + state.dx) * mindim;
   y = (-.1 + state.dy) * mindim;
   r = .01 * mindim;
 
-  if (Math.hypot(x, y) < mindim/2) {
+  if (Math.hypot(x, y) < mindim / 2) {
     ctx.moveTo(x + r, y);
-    ctx.arc(x, y, r, 0, Math.PI * 2);  
+    ctx.arc(x, y, r, 0, Math.PI * 2);
   }
   // x-=.5*mindim;
   // y+=.44*mindim;
@@ -102,19 +79,13 @@ function mainLoop(now) {
   ctx.fillStyle = "lightgray";
   ctx.arc(0, 0, pr, 0, Math.PI * 2);
   ctx.fill();
-  
+
   ctx.beginPath();
   ctx.strokeStyle = "black";
   ctx.moveTo(0 + 0.5 * mindim, 0);
   ctx.arc(0, 0, 0.5 * mindim, 0, Math.PI * 2);
   ctx.stroke();
-
-  // TODO: switch back
-  // world can create entities
-  // display can draw them
-  // they both share player position and velocity
-  //World.update(state, dt); // update entities
-  //Display.draw(state, state.ctx); // draw entities
+  /* display */
 
   if (state.isQuit) return console.log("quit");
 
@@ -139,8 +110,8 @@ function main() {
   document.body.appendChild(canvas); // add to body
 
   // TODO: switch back to the standard state initializer...
-  //const state = World.create(canvas,ctx); // initialize!
-  const state = { canvas: canvas, ctx: ctx }; // minimum requirement
+  const state = World.create(canvas,ctx); // initialize!
+  // const state = { canvas: canvas, ctx: ctx }; // minimum requirement
   state.inputs = inputs; // from events.js
   document.state = state;
 
