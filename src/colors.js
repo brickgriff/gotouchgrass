798bf91326e10a9d54349ab340023c95c920fd96
bgriffiniv <bgriffiniv@gmail.com>
2026-04-05 18:51:16 -0400

    named colors

--- src/colors.js
+++ src/colors.js
@@ -1,8 +1,17 @@
 // colors.js
 
+const namedColors = {
+  rose: '#fe6f61',
+  pink: '#f7cac8',
+  plum: '#5f4c8b',
+  soil: '#8b5f4c',
+  sand: '#f7e9c8',
+  lawn: '#88b24b',
+};
+
 export const colors = {
 
-  background: '#8b5f4c', // '#88b24b',
-  player: '#5f4c8b',
+  background: namedColors.soil, // '#8b5f4c', // '#88b24b',
+  player: namedColors.plum, // '#5f4c8b',
 
 };
